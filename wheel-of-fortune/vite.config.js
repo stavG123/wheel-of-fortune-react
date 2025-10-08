fd8830036f26410c38ef6f65cb4d6f13ded19069
Stav Gorobets <gorobetss1@lasalle.edu>
2025-10-07 20:51:14 -0400

    s

--- wheel-of-fortune/vite.config.js
+++ wheel-of-fortune/vite.config.js
@@ -1,3 +1,14 @@
-import { defineConfig } from 'vite' 
-import react from '@vitejs/plugin-react'// https://vite.dev/config/ 
-export default defineConfig({ plugins: [ react({ babel: { plugins: [['babel-plugin-react-compiler']], }, }), ], })
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: '/wheel-of-fortune-react/',  // 👈 important for GitHub Pages
+  plugins: [
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler']],
+      },
+    }),
+  ],
+})
